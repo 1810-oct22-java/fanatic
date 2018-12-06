4be75e1f3747150488e178103749b4071664ef1d
Phil Gerringer <pgerringer@gmail.com>
2018-12-05 21:13:00 -0500

    more changes....

--- MovieFanatics/src/main/java/com/fanatics/services/ReviewService.java
+++ MovieFanatics/src/main/java/com/fanatics/services/ReviewService.java
@@ -6,14 +6,21 @@ package com.fanatics.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.fanatics.beans.Review;
 
 /**
  * @author PGerringer
  *
  */
+@Service("reviewService")
 public class ReviewService {
 	static ArrayList<Review> reviews = new ArrayList<Review>();
+	static {
+		reviews.add(new Review(1, 1, 3));
+		reviews.add(new Review(2, 1, 5));
+	}
 
 	/**
 	 * 
@@ -24,5 +31,10 @@ public class ReviewService {
 
 	public List<Review> getAll(){
 		return reviews;
+	}
+	
+	public Review getById(int id) {
+		return reviews.stream().filter(t -> t.getReview_id()==id).findFirst()
+				.orElse(null);
 	}
 }
